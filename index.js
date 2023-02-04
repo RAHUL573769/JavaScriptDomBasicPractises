340bdd9a90262663f676399527bfe2d2ea25b044
Rahul Rudra <rahulrudra665@gmail.com>
2023-02-04 09:44:08 +0600

    styles added

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const fruits=document.getElementsByClassName("fruitsname");
 
 for(const fruit of fruits){
     console.log(fruit.innerText);
-    fruit.innerText="Changed";
+    // fruit.innerText="Changed";
 }
 
 console.log(fruits);
@@ -31,3 +31,26 @@ console.log(lists);
 //------------------------------------------Styling Using Dom=----------------------------------------------------
 
 name1.style.backgroundColor="yellow";
+
+const classList=name1.classList;
+console.log(classList);
+name1.setAttribute("title","Tooltip by Js");
+
+const nams=name1.getAttribute("id");
+console.log(nams);
+
+
+
+
+
+
+const sectionList=document.getElementsByTagName("section");
+console.log(sectionList);
+
+for(const section of sectionList){
+    console.log(section);
+
+    section.style.border="5px solid green";
+    section.style.margin="10px";
+    section.style.backgroundColor="lightblue";
+}
